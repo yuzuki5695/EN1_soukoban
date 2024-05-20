ef85279920dcb3122913cee258fa70fcedf497a9
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-05-20 10:59:39 +0900

    EN1_01_02

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -7,54 +7,71 @@ public class GameManagerScript : MonoBehaviour
     //配列の宣言
     int[] map;
 
-     void Start()
+    //クラスの中、メソッドの外に書くことに注意
+    void PrintArray()
     {
-        map = new int[] {0,0,0,1,0,0,0,0,0};
         string debugText = "";
-
-        //デバックログの出力
-        //Debug.Log("Hello world");//削除
-        for(int i = 0; i < map.Length; i++)
+        for (int i = 0; i < map.Length; i++)
         {
-            //文字列を結合
-            debugText += map[i].ToString() + ",";
-
+            debugText += map[i].ToString() + ", ";
         }
-        //文字列を出力
-        Debug.Log(debugText);
-
+        Debug.Log(debugText);  
     }
 
+    // クラスの中、メソッドの外に書くことに注意
+    // 返り値の型の注意
+    int GetPlayerIndex()
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (map[i] == 1)
+            {
+                return 1;
+            } 
+        }
+        return -1;
+    }
+    // クラスの中、メソッドの外に書くことに注意
+    // 返り値の型の注意
+    bool MoveNumber(int number,int moveFrom,int moveTo)
+    {
+        // 移動先が範囲外なら移動不可
+        if (moveTo < 0 || moveTo >= map.Length) { return false; }
+        // 移動先に2(箱)がいたら
+        if (map[moveTo] == 2)
+        {
+            // どの方向へ移動するかを算出
+            int velocity = moveTo - moveFrom;
+            // プレイヤーの移動先から、さらに先に2(箱)を移動
+            // 移動処理のメソッドを呼び、処理を再起。
+            // 移動可不可をboolで記録
+            bool success = MoveNumber(2, moveTo, moveTo + velocity);
+            // 箱が移動失敗したら、プレイヤーの移動も失敗
+            if (!success) { return false; }
+        }
+        // プレイヤー ・ 箱関わらずの移動処理       
+        map[moveTo] = number;
+        map[moveFrom] = 0;
+        return true;
+    }
+
+    void Start()
+    {
+        map = new int[] {0,0,0,1,0,2,0,0,0};
+        PrintArray();
+
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.RightParen))
         {
-            int PlayerIndex = -1;
-            //要素はmap.Lengthで取得
-            for (int i = 0; i < map.Length; i++)
-            {
-                if (map[i] == 1)
-                {
-                    PlayerIndex = i;
-                    break;
-                }
-            }
-
-            if (PlayerIndex < map.Length - 1)
-            {
-
-                map[PlayerIndex + 1] = 1;
-                map[PlayerIndex] = 0;
-
-            }
-
-            string debugText = "";
-            for (int i = 0; i < map.Length; i++)
-            {
-                debugText += map[i].ToString() + ", ";
-            }
-            Debug.Log(debugText);
+            // メソッド化した処理を使用
+            int PlayerIndex = GetPlayerIndex();
+            // 移動処理を関数化
+            MoveNumber(1, PlayerIndex, PlayerIndex + 1);
+            PrintArray();
+            
         }
     }
 }
