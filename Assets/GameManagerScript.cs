b40643f0eb2435502057c98366bfa9af764477a4
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-05-20 11:06:57 +0900

    EN1_01_02

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Claims;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManagerScript : MonoBehaviour
 {
